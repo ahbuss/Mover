5a62dcde15ea22ad5f50f6f2b7ef1649b277b6a3
Buss, Arnold <abuss@nps.edu>
2018-04-02 14:31:21 -0700

    Initial Netbeans project for MV3302

---
+++ src/mv3302/HelloMV3302.java
@@ -0,0 +1,16 @@
+package mv3302;
+
+/**
+ *
+ * @author ahbuss
+ */
+public class HelloMV3302 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("A placeholder");
+    }
+
+}
